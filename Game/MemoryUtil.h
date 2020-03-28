13b97c8807fd4ed0c96b383dcf7a555ae96df0ba
David Marcec <dmarcecguzman@gmail.com>
2020-03-28 16:40:06 +1100

    Added AddressCall which allows you to call an address without defining a type

--- Game/MemoryUtil.h
+++ Game/MemoryUtil.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstring>
+#include <type_traits>
 #include <Windows.h>
 extern "C" {
 #include "ld32.h"
@@ -36,6 +37,41 @@ void PatchType(DWORD address, T data) {
 template <typename T>
 T MakeCallable(DWORD address) {
     return reinterpret_cast<T>(address);
+}
+
+enum class CallConvention { CDecl, ClrCall, StdCall, FastCall, ThisCall, VectorCall };
+template <CallConvention C, typename T, typename... ARGS>
+struct BuildCallable {
+    constexpr static auto value() {
+        if constexpr (C == CallConvention::CDecl) {
+            return static_cast<T(_cdecl*)(ARGS...)>(nullptr);
+        } else if constexpr (C == CallConvention::ClrCall) {
+#ifndef __cplusplus_cli
+            static_assert(false, "Requires /clr or /ZW command line option");
+#else
+            return static_cast<T(__clrcall*)(ARGS...)>(nullptr);
+#endif
+        } else if constexpr (C == CallConvention::StdCall) {
+            return static_cast<T(__stdcall*)(ARGS...)>(nullptr);
+        } else if constexpr (C == CallConvention::FastCall) {
+            return static_cast<T(__fastcall*)(ARGS...)>(nullptr);
+        } else if constexpr (C == CallConvention::ThisCall) {
+            return static_cast<T(__thiscall*)(ARGS...)>(nullptr);
+        } else if constexpr (C == CallConvention::VectorCall) {
+            return static_cast<T(__vectorcall*)(ARGS...)>(nullptr);
+        } else {
+            static_assert(false, "Invalid call convention");
+        }
+    }
+};
+template <CallConvention C, typename R, typename... ARGS>
+R AddressCall(DWORD address, ARGS... args) {
+    using CALLABLE = decltype(BuildCallable<C, R, ARGS...>::value());
+    if constexpr (std::is_void_v<R>) {
+        reinterpret_cast<CALLABLE>(address)(args...);
+    } else {
+        return reinterpret_cast<CALLABLE>(address)(args...);
+    }
 }
 
 template <typename T>
