53fa8ae390564be2e3acfd805a01f0d8b0a60704
David Marcec <dmarcecguzman@gmail.com>
2020-04-09 21:41:58 +1000

    VMT Hooking

--- Game/MemoryUtil.h
+++ Game/MemoryUtil.h
@@ -141,4 +141,24 @@ T CreateDetour(uintptr_t src, uintptr_t dst) {
     return reinterpret_cast<T>(og_func);
 }
 
+template <std::size_t VMTOffset>
+void PatchVMTMethod(uintptr_t base, uintptr_t dst) {
+    const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
+
+    ReprotectScope<MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
+    PatchType<uintptr_t>(method_offset, dst);
+}
+
+template <typename T, std::size_t VMTOffset>
+T CreateVMTMethodDetour(uintptr_t base, uintptr_t dst) {
+    const uintptr_t method_offset = base + (VMTOffset * sizeof(uintptr_t));
+
+    ReprotectScope<MemPerm::ReadWriteExecute> protection(method_offset, sizeof(uintptr_t));
+
+    auto original = ReadType<uintptr_t>(method_offset);
+    PatchType<uintptr_t>(method_offset, dst);
+
+    return reinterpret_cast<T>(original);
+}
+
 } // namespace MemoryUtil
